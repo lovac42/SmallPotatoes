3b1fd82aaa001104988d0b3f9f89ba1bdff39ade
lovac42 <lovac42@protonmail.com>
2020-04-02 14:17:37 -0400

    Fix erroneous values containing float points in 2.1.24beta3

--- src/small_potatoes/small_potatoes.py
+++ src/small_potatoes/small_potatoes.py
@@ -10,35 +10,31 @@ from anki.hooks import wrap
 
 from anki import version
 ANKI21 = version.startswith("2.1.")
+if ANKI21:
+    unicode = str
 
 
 def listToUser(deckConf, steps, _old):
     arr=[]
     for i in steps:
         if i>=1440 and i%1440==0:
-            i=str(i//1440)+'d'
+            i=str(int(i//1440))+'d'
         arr.append(str(i))
     return " ".join(arr)
 
 
 def updateList(deckConf, conf, key, w, minSize=1, _old=None):
-    if ANKI21:
-        steps=str(w.text()).split(" ")
-    else: #for older python 2.6 and 2.7
-        steps=unicode(w.text()).split(" ")
-
     arr=[]
+    steps=unicode(w.text()).split(" ")
     for i in steps:
         if not i: continue
         if i.endswith('d'):
-            i=i[:-1]
+            i=float(i[:-1])
             i=int(i)*1440
         arr.append(i)
     w.setText(" ".join([str(x) for x in arr]))
-    _old(deckConf, conf, key, w, minSize)
+    return _old(deckConf, conf, key, w, minSize)
 
 
 aqt.deckconf.DeckConf.updateList = wrap(aqt.deckconf.DeckConf.updateList, updateList, pos="around")
 aqt.deckconf.DeckConf.listToUser = wrap(aqt.deckconf.DeckConf.listToUser, listToUser, pos="around")
-
-
